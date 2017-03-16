683eaa5c6563db2944df8d1e435cb78d83e8ceec
Nur Muhamad Holik <nholik1205@gmail.com>
2017-03-16 18:30:57 +0800

    add file

---
+++ application/views/rekap/capaian_view.php
@@ -0,0 +1,7 @@
+<?php $this->load->view('header')?>
+<body>
+<?php $this->load->view('rekap/bodycapaian')?>  
+<?php $this->load->view('footer')?>    
+<?php $this->load->view('rekap/js_capaian_kinerja')?> 
+</body>
+</html>
