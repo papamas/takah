7958bf87100b2570fc19ff2971ef02f7f7542fc4
papamas <nurmuhamadh@gmail.com>
2017-03-17 15:36:44 +0800

    fix laporan peminjaman

---
+++ application/views/laporan/vpinjam.php
@@ -0,0 +1,7 @@
+<?php $this->load->view('header')?>
+<body>
+<?php $this->load->view('laporan/bodypinjam')?>  
+<?php $this->load->view('footer')?>    
+<?php $this->load->view('laporan/js_pinjam')?>  
+</body>
+</html>
